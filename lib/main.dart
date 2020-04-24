f1b20e85f5c471028e60d63c8fd8bab557deec61
prashantsrivastava23 <prashantsrivastava23@outlook.com>
2020-04-25 03:36:39 +0530

    Initial Commit - Login Page, SignUp Page, Associations Page

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+import 'signup.dart';
+import 'myassociation.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: <String, WidgetBuilder>{
+        '/signup': (BuildContext context) => new SignupPage(),
+        '/myassociations': (BuildContext context) => new MyAssociationPage()
+      },
+      home: new LoginPage(),
+    );
+  }
+}
+
